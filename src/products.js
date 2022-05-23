dfc8d7b37efa4b8a2d920e2dfa96436a7be7c489
sanderscody26 <sanderscody26@gmail.com>
2022-05-23 12:31:16 -0400

    Made a products file with object to act as a database for testing

---
+++ src/products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    _id: '1',
+    name: 'product 1',
+    image: '',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    brand: 'GA',
+    category: 'Books',
+    price: 12.99,
+    countInStock: 20,
+    rating: 1,
+    numReviews: 4
+  },
+  {
+    _id: '2',
+    name: 'product 2',
+    image: '',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    brand: 'Polar Express',
+    category: 'Movie',
+    price: 24.99,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 2
+  },
+  {
+    _id: '3',
+    name: 'product 4',
+    image: '',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    brand: 'WOW',
+    category: 'Books',
+    price: 12.99,
+    countInStock: 20,
+    rating: 1,
+    numReviews: 4
+  },
+  {
+    _id: '4',
+    name: 'product 4',
+    image: '',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    brand: 'Airpros',
+    category: 'Electronics',
+    price: 249.99,
+    countInStock: 400,
+    rating: 4,
+    numReviews: 10
+  }
+]
+
+export default products
